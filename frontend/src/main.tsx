e980d3e1365cffe26943ea7fdcbde12133486887
Ayose <ayosec@gmail.com>
2025-01-31 00:00:00 +0000

    frontend: use custom fonts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,9 @@ import App from "./App.tsx";
 import initTooltips from "./tooltips.ts";
 import { BackendProvider } from "./backend";
 
+import "@fontsource-variable/quicksand/index.css";
+import "@fontsource-variable/roboto-mono/index.css";
+
 import "./root.css";
 import "./theme.css";
 
