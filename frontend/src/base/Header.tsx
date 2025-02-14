c6395d5ce99d9061141a21c5f4b157b3ad59793a
Ayose <ayosec@gmail.com>
2025-02-14 00:00:00 +0000

    frontend: animate colors during theme transition

--- frontend/src/base/Header.tsx
+++ frontend/src/base/Header.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+
+import { MdOutlineDarkMode, MdOutlineLightMode } from "react-icons/md";
 
 import styles from "./header.module.css";
-import { MdOutlineDarkMode, MdOutlineLightMode } from "react-icons/md";
 
 enum Theme {
     Dark = "dark",
@@ -18,15 +19,31 @@ function getColorScheme() {
 function ThemeSwitcher() {
     const [ theme, setTheme ] = useState(getColorScheme);
 
+    const animationTask = useRef<ReturnType<typeof setTimeout>|null>(null);
+
     const toggle = () => {
         setTheme(theme === Theme.Dark ? Theme.Light : Theme.Dark);
+
+        if (animationTask.current !== null)
+            clearTimeout(animationTask.current);
+
+        animationTask.current = setTimeout(
+            () => {
+                animationTask.current = null;
+                document.body.classList.remove(styles.animatedColors);
+            },
+            500,
+        );
+
+        document.body.classList.add(styles.animatedColors);
     };
 
     return (
         <div
             id="selected-theme"
-            className={styles.themeSwitcher}
+            className={styles.optionSwitcher}
             data-select-theme={theme}
+            data-option-highlight={theme === Theme.Light ? "right" : "left"}
             onClick={toggle}
         >
             <MdOutlineDarkMode size="10px" />
@@ -36,7 +53,6 @@ function ThemeSwitcher() {
 }
 
 export default function Header() {
-
     return (
         <header className={styles.header}>
             <div className={styles.title}>FFmpeg - drawvg</div>
